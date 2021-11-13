2e3cab8b487caa6281b204e7b1d3da023f921d18
Khalil <xermed@yandex.ru>
2021-11-13 23:52:06 +0300

    Added task_6_5

---
+++ home_works/batkaev_khalil_dz_6/add_sale_adv.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+if len(argv) <= 1:
+    print('Введите номер позиции для замены и сумму продаж')
+else:
+    command, mark, sale_amount, *args = argv
+    mark = int(mark)
+    id_line = 0
+    with open('bakery_adv.csv', 'r+', encoding='utf-8') as f:
+        if mark == 1:
+            f.seek(0)
+            f.write(f"{sale_amount:<30} \n")
+            exit(0)
+        else:
+            line = f.readline()
+            while line:
+                id_line += 1
+                if mark - id_line == 1:
+                    f.seek(f.tell())
+                    f.write(f"{sale_amount:<15} \n")
+                    exit(0)
+                line = f.readline()
