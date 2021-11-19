2df2c022ddc45caa9ff6c8f9592c7a6c734caf25
Khalil <xermed@yandex.ru>
2021-11-19 19:50:07 +0300

    added new ver task_7_5

--- batkaev_khalil_dz_7/task_7_5/task_7_5.py
+++ batkaev_khalil_dz_7/task_7_5/task_7_5.py
@@ -7,15 +7,16 @@ result_dict = {}
 left_limit, right_limit, step, size_max = 0, 100, 10, 100000
 
 while right_limit <= size_max:
-    ext_list = list()
+    ext_set = set()
+    count = 0
     for file in scandir(dir_path):
         if left_limit <= stat(file).st_size < right_limit:
             ext = file.name.rsplit('.', maxsplit=1)[-1]
-            ext_list.append(ext)
-    if ext_list:
-        quantity = len(ext_list)
-        unique_ext_list = list(set(ext_list))
-        result_dict[right_limit] = [quantity] + [unique_ext_list]
+            ext_set.add(ext)
+            count += 1
+    if ext_set:
+        ext_list = list(ext_set)
+        result_dict[right_limit] = [count] + [ext_list]
     left_limit = right_limit
     right_limit *= step
 
