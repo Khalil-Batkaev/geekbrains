efde3e36a205070f61650af7b6eb322a8fe6d736
Khalil <xermed@yandex.ru>
2021-11-28 23:44:58 +0300

    new ver task_10_2

--- batkaev_khalil_dz_10/task_10_2.py
+++ batkaev_khalil_dz_10/task_10_2.py
@@ -22,10 +22,6 @@ class Clothes(ABC):
         pass
 
     @property
-    def clear_reserved_fabric(self):
-        self.__reserved_fabric.clear()
-
-    @property
     def get_size(self):
         return self.__size
 
@@ -48,37 +44,13 @@ class Costume(Clothes):
         return self.__result
 
 
-coat_1, coat_2, coat_3, coat_4 = Coat(48), Coat(56), Coat(62), Coat(70)
-costume_1, costume_2, costume_3, costume_4 = Costume(188), Costume(176), Costume(180), Costume(168)
+coat_1, coat_2, coat_3 = Coat(48), Coat(56), Coat(62)
+costume_1, costume_2, costume_3 = Costume(188), Costume(176), Costume(180)
 
-print(coat_1.fabric_calculation)
-print(costume_2.fabric_calculation)
-total_fabric_for_coats = coat_1 + coat_2 + coat_3 + coat_4
-print(total_fabric_for_coats)
+print(coat_1.fabric_calculation, coat_2.fabric_calculation, coat_3.fabric_calculation, sep='\n')
+print()
+print(costume_1.fabric_calculation, costume_2.fabric_calculation, costume_3.fabric_calculation, sep='\n')
+print()
+total_fabric_for_coats = coat_1 + coat_2 + coat_3
 total_fabric_for_costumes = costume_1 + costume_2 + costume_3
-print(total_fabric_for_costumes)
-new_total_fabric_for_costumes = total_fabric_for_costumes + costume_4
-print(new_total_fabric_for_costumes)
-
-total_fabric_for_costumes.clear_reserved_fabric
-total_fabric_for_costumes = costume_1 + costume_4
-print(total_fabric_for_costumes)
-
-'''
-Можно так методы добавить в класс-родитель вместо перегрузки __str__ и __add__, но тогда вызов итоговой цифры идёт 
-через экземпляр, а это мне не очень нравится...
-
-    # @property
-    # def add_to_reserve(self):
-    #     self.__reserved_fabric.append(self.fabric_calculation)
-    #
-    # @property
-    # def show_reserved_fabric(self):
-    #     return sum(self.__reserved_fabric)
-    
-    coat_1.add_to_reserve
-    coat_2.add_to_reserve
-    coat_3.add_to_reserve
-    coat_4.add_to_reserve
-    coat_1.show_reserved_fabric    
-'''
+print(total_fabric_for_coats, total_fabric_for_costumes, sep='\n')
