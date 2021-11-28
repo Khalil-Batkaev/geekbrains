4ee3ad98e61747154b1d936494ebc8bfddb14f7f
Khalil <xermed@yandex.ru>
2021-11-28 23:05:37 +0300

    added task_10_3

---
+++ batkaev_khalil_dz_10/task_10_3.py
@@ -0,0 +1,51 @@
+class Cell:
+
+    def __init__(self, quantity):
+        self.__quantity = quantity
+
+    def __str__(self):
+        return str(self.__quantity)
+
+    def __add__(self, other):
+        result = self.__quantity + other.__quantity
+        return Cell(result)
+
+    def __sub__(self, other):
+        if self.__quantity >= other.__quantity:
+            result = self.__quantity - other.__quantity
+        else:
+            raise ValueError('Начальное количество клеток меньше вычитаемого')
+        return Cell(result)
+
+    def __mul__(self, other):
+        result = self.__quantity * other.__quantity
+        return Cell(result)
+
+    def __floordiv__(self, other):
+        result = self.__quantity // other.__quantity
+        return Cell(result)
+
+    def make_order(self, quantity):
+        rows = self.__quantity // quantity
+        remainder = self.__quantity % quantity
+        if not remainder:
+            line = ('*' * quantity + '\\n') * (rows - 1) + '*' * quantity
+        else:
+            line = ('*' * quantity + '\\n') * rows + '*' * remainder
+        return line
+
+
+cell_1 = Cell(5)
+cell_2 = Cell(10)
+cell_3 = cell_1 + cell_2
+cell_4 = cell_2 - cell_1
+cell_5 = cell_1 * cell_2
+cell_6 = cell_2 // cell_1
+print(cell_3, cell_4, cell_5, cell_6, sep='\n')
+print(cell_3.make_order(5))
+print(cell_5.make_order(15))
+
+try:
+    cell_4 = cell_1 - cell_2
+except ValueError as e:
+    print(e)
